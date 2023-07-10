5f6cd2e0c121361476b435723f2c84f688e26c76
Haseeb Zafar <haseebzafar98@gmail.com>
2023-07-10 23:10:12 +0300

    Update api_config.dart

--- lib/data/source/api/api_config.dart
+++ lib/data/source/api/api_config.dart
@@ -8,6 +8,34 @@ class ApiConfig {
 
   late Dio dio;
 
+    // Global options
+  final options = CacheOptions(
+    // A default store is required for interceptor.
+    store: MemCacheStore(),
+
+    // All subsequent fields are optional.
+
+    // Default.
+    policy: CachePolicy.forceCache,
+    // Returns a cached response on error but for statuses 401 & 403.
+    // Also allows to return a cached response on network errors (e.g. offline usage).
+    // Defaults to [null].
+    hitCacheOnErrorExcept: [401, 403],
+    // Overrides any HTTP directive to delete entry past this duration.
+    // Useful only when origin server has no cache config or custom behaviour is desired.
+    // Defaults to [null].
+    maxStale: const Duration(days: 7),
+    // Default. Allows 3 cache sets and ease cleanup.
+    priority: CachePriority.normal,
+    // Default. Body and headers encryption with your own algorithm.
+    cipher: null,
+    // Default. Key builder to retrieve requests.
+    keyBuilder: CacheOptions.defaultCacheKeyBuilder,
+    // Default. Allows to cache POST requests.
+    // Overriding [keyBuilder] is strongly recommended when [true].
+    allowPostMethod: false,
+  );
+
   ApiConfig._internal() {
     //creates the singleton instance
     dio = Dio();
